4a02a3ccc18f45215f6df609798b0d817b6998e6
mrsnowball88 <38319320+mrsnowball88@users.noreply.github.com>
2023-03-17 16:24:53 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     $('.clients-slider').slick({
-        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: false,
@@ -8,4 +7,18 @@ $( document ).ready(function() {
         nextArrow: '<button type="button" class="slick-nav slick-next"><img src="./img/clients/next.png" alt=""></button>',
         prevArrow: '<button type="button" class="slick-nav  slick-prev"><img src="./img/clients/prev.png" alt=""></button>'
       });
+      $(".accordeon-item-title").click(function(){
+        var acc_body = $(this).parent(".accordeon-item").find('.accordeon-item-body');
+        var acc_toggler = $(this).parent(".accordeon-item").find('span');
+        if(acc_toggler.html() == "+"){
+          acc_toggler.html('-')
+        }else {
+          acc_toggler.html('+')
+        }
+        //$('.accordeon-item-body').not($(this).next()).slideUp(400);
+        
+        //$('.accordeon-item-title span').not($(this).next()).html('+')
+        
+        $(this).next().slideToggle(400);
+      })
 });
